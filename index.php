3a73d249db73a85d4efaea27f87494d81f4232c8
Oscar Crespo <crespo.krw@gmail.com>
2015-09-01 03:15:32 -0500

    adaptacion y actualizacion

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+	<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>Punto & Guion</title>
+		<link href='http://fonts.googleapis.com/css?family=Lato:100,400,700' rel='stylesheet' type='text/css'>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="shared/js/minified/TweenLite.min.js"></script>
+		<script src="shared/js/minified/TimelineMax.min.js"></script>
+		<script src="shared/js/minified/easing/EasePack.min.js"></script>
+		<script src="shared/js/minified/plugins/CSSPlugin.min.js"></script>
+		<script src="shared/js/main.js"></script>
+		<link rel="stylesheet" href="shared/css/main.css">
+		<script src="shared/js/xspMin-1.2.js"></script>
+
+	</head>
+	<body id="body" class="bg3">
+		<div id="showcaser">
+			
+			<div id="mssBienvenida">
+					<div class="a1">Creando</div>
+					<div class="a2">experiencias</div>
+					<div class="a3">maravillosas</div>
+			</div>
+
+		</div>
+
+		<header>
+			<a href="index.php"><h1 id="logo"></h1></a>
+
+			<nav>
+				<a href=""><div class="btn"><span>About</span></div></a>
+				<a href="http://www.behance.net/puntoyguion"><div class="btn" ><span>Portafolio</span></div></a>
+				<a href=""><div class="btn" ><span>Contacto</span></div></a>
+			</nav>
+
+		</header>
+			
+			
+		
+		<section id="index"></section>
+		
+		
+	</body>
+	</html>	
