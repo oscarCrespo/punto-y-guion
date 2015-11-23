226ae153089cea335f2a7f7924ed6e9c2cc1e733
Oscar Crespo <crespo.krw@gmail.com>
2015-11-23 03:23:40 -0600

    projects section done

--- index.php
+++ index.php
@@ -40,9 +40,214 @@
 	</section>
 	<section id="projects">
 		<div id="itemsWrapper">
-			<div class="item">
-				<img src="shared/img/projects/ciudadDeLasIdeas.jpg" alt="">
-			</div>
+		<a href="http://www.aprendeycrece.com.mx/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/aprendeYCrece.jpg" alt="Aprende y crece">
+					<span class="mask"></span>
+					<h5 class="brand">Aprende y Crece</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Responsive Arquitecture</span>
+						<span class="tag tit">Development</span>
+						<span class="tag">Responsive and liquid layout</span>
+						<span class="tag">Cross browser</span>
+						<span class="tag">Multiligual</span>
+						<span class="tag">E-learning platform</span>
+						<span class="tag">Social API</span>
+						<span class="tag">Accesibility</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Custom components</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://www.ciudaddelasideas.com/giftedcitizen/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/giftedCitizen.jpg" alt="CDI - Gifted Citizen">
+					<span class="mask"></span>
+					<h5 class="brand">CDI - Gifted Citizen</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag tit">Responsive Arquitecture</span>
+						<span class="tag">Templating</span>
+						<span class="tag">JS animations</span>
+						<span class="tag">Responsive and liquid layout</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Social APIs</span>
+						<span class="tag">Multilingual</span>
+						<span class="tag">Scroll animation</span>
+						<span class="tag">Multimedia Platform</span>
+					</div>
+				</div>
+			</a>
+		
+			<a href="http://www.aramark.com.mx/index.php" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/aramark.jpg" alt="Aramark">
+					<span class="mask"></span>
+					<h5 class="brand">Aramark</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag">Custom double slider</span>
+						<span class="tag">Interaction design</span>
+						<span class="tag">Scroll interaction</span>
+						<span class="tag">Custom layout</span>
+						<span class="tag">Liquid layout</span>
+						<span class="tag">Static site</span>
+					</div>
+				</div>
+			</a>
+			<a href="https://www.huggies.com.mx/site/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/huggies.jpg" alt="Huggies">
+					<span class="mask"></span>
+					<h5 class="brand">Huggies</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag">Mobile site</span>
+						<span class="tag">Responsive and liquid layout</span>
+						<span class="tag">Liquid layout</span>
+						<span class="tag">Retina ready</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Interaction design</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://www.tajin.com/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/tajin.jpg" alt="Ciudad de las Ideas">
+					<span class="mask"></span>
+					<h5 class="brand">Tajin</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag">Custom components</span>
+						<span class="tag">Parallax scrolling</span>
+						<span class="tag">Custom layout</span>
+						<span class="tag">Social Networks APIs</span>
+						<span class="tag">Column style layout</span>
+						<span class="tag">Multilingual</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Desktop and mobile development</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://www.agenciai.mx/index.php" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/ilatina.jpg" alt="Agencia i">
+					<span class="mask"></span>
+					<h5 class="brand">Agencia i</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag tit">UX</span>
+						<span class="tag">Responsive and liquid layout</span>
+						<span class="tag">CSS and js Animaiton</span>
+						<span class="tag">Embedded video</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Scroll interaction</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://www.ciudaddelasideas.com/caricaturas/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/caricaturas.jpg" alt="CDI - Caricaturas">
+					<span class="mask"></span>
+					<h5 class="brand">CDI - Caricaturas</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag tit">Interaction</span>
+						<span class="tag">Templating</span>
+						<span class="tag">Drag and Drop component</span>
+						<span class="tag">Responsive and liquid layout</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Custom share component</span>
+						<span class="tag">Scroll animation</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://www.hemisferiofinanciero.com/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/hemisferio.jpg" alt="Hemisferio Financiero">
+					<span class="mask"></span>
+					<h5 class="brand">Hemisferio Financiero</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Brand Design</span>
+						<span class="tag tit">UI Design</span>
+						<span class="tag tit">Development</span>
+						<span class="tag tit">UX</span>
+						<span class="tag">Single page layout</span>
+						<span class="tag">Liquid layout</span>
+						<span class="tag">Custom js components</span>
+						<span class="tag">Static site</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://www.ciudaddelasideas.com/acercaDe.php" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/ciudadDeLasIdeas.jpg" alt="Ciudad de las Ideas">
+					<span class="mask"></span>
+					<h5 class="brand">Ciudad de las ideas</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag">Custom components</span>
+						<span class="tag">Drag and Drop</span>
+						<span class="tag">js animation</span>
+						<span class="tag">Custom layout</span>
+						<span class="tag">You tube API</span>
+						<span class="tag">Social Networks APIs</span>
+						<span class="tag">CSS animation</span>
+						<span class="tag">Multilingual</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">Multimedia Platform</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://mabakerandchef.com/index.php" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/maBaker.jpg" alt="Ciudad de las Ideas">
+					<span class="mask"></span>
+					<h5 class="brand">Ma Baker and Chef</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">UI Design</span>
+						<span class="tag tit">Development</span>
+						<span class="tag">Responsive Slider</span>
+						<span class="tag">Parallax scrolling</span>
+						<span class="tag">Youtube API</span>
+						<span class="tag">Multilingual</span>
+						<span class="tag">Templating</span>
+						<span class="tag">Product catalog</span>
+						<span class="tag">Responsive and liquid layout</span>
+					</div>
+				</div>
+			</a>
+			<a href="https://www.ninshionline.com/" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/ninshi.jpg" alt="Ciudad de las Ideas">
+					<span class="mask"></span>
+					<h5 class="brand">Ninshi online</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Development</span>
+						<span class="tag">Custom components</span>
+						<span class="tag">E-commerce platform</span>
+						<span class="tag">js animation</span>
+						<span class="tag">Custom layout</span>
+						<span class="tag">You tube API</span>
+						<span class="tag">Dynamic site</span>
+						<span class="tag">E-learning platform</span>
+					</div>
+				</div>
+			</a>
+			<a href="http://taner.com.mx/index.php" target="_blank">
+				<div class="item">
+					<img src="shared/img/projects/taner.jpg" alt="Ciudad de las Ideas">
+					<span class="mask"></span>
+					<h5 class="brand">Taner</h5>
+					<div class="tagsGroup">
+						<span class="tag tit">Brand Design</span>
+						<span class="tag tit">UI design</span>
+						<span class="tag tit">Development</span>
+						<span class="tag">Liquid layout</span>
+						<span class="tag">Product catalog</span>
+					</div>
+				</div>
+			</a>
 		</div>
 	</section>
 	<section id="contacto">
