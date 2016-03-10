23c6d57f487c193919147fd528045ac1dc7c6dff
Oscar Crespo <crespo.krw@gmail.com>
2016-03-10 16:31:42 -0500

    js Refactory, buttons added and icon

--- shared/js/main.js
+++ shared/js/main.js
@@ -3,60 +3,96 @@ smoothScroll.init({
 	"easing": "easeInOutCubic"
 });
 
+var $img = $("#bgSequence");
+var $win = $(window);
 
-(function($){
+function documentDimensions() {
+	globalVars.$winWidth = $win.width();
+	globalVars.$winHeight = $win.height();
+}
+documentDimensions();
 
-	var $img = $("#bgSequence");
+window.onresize = function(){
+	documentDimensions();
+}
 
-	function updateImg() {
-		var newNumber;
-		if( counter <= frames-1 && forward){
-			newNumber = pad(counter++, 4);
-			if (counter == frames-1) forward=false;
-		}else if(counter < frames && !forward ) {
-			newNumber = pad(counter--, 4);
-			if (counter == 0) forward=true;
+function mouseMoveOn(){
+	$(document).on( 'mousemove', mouseListener );
+}
+//mouseMoveOn();
+
+function mouseMoveOff(){
+	$(document).unbind('mousemove', mouseListener);
+}
+
+function mouseListener() {
+	var xPercent = (event.pageX / globalVars.$winWidth) * 100;
+	var yPercent = (event.pageY / globalVars.$winHeight) * 100;
+
+	mouseMoveFunctions( xPercent, yPercent );
+}
+
+function mouseMoveFunctions( x, y ) {
+	var floorX = Math.floor(x * 0.80);
+	var floorY = Math.floor(y * 0.1);
+	console.log(x, y);
+
+	globalVars.counter = floorX ;
+
+	updateImg()
+}
+
+function updateImg() {
+	var newNumber;
+	if( globalVars.counter <= globalVars.frames-1 && globalVars.forward){
+		newNumber = pad(globalVars.counter++, 4);
+		if (globalVars.counter == globalVars.frames-1) globalVars.forward = false;
+	}else if(globalVars.counter <= globalVars.frames && !globalVars.forward ) {
+		newNumber = pad(globalVars.counter--, 4);
+		if ( globalVars.reverseOnce && globalVars.counter == 0) {
+			clearInterval(interval);
+			globalVars.reverseOnce = false;
 		}
-		console.log( counter );
-		var newImg = prefix + newNumber + ".jpg";
-		$img.attr("src", newImg);
+		if (globalVars.counter == 0) globalVars.forward = true;
 	}
+	//console.log( globalVars.counter );
+	var newImg = globalVars.prefix + newNumber + ".jpg";
+	$img.attr("src", newImg);
+}
+function frameUpdate(forward, stop) {
+	var newFrame;
+	if ( globalVars.counter == stop) { 
+		clearInterval(interval);
+		return
+	}
+	if (forward) {
+		newFrame = pad(globalVars.counter++, 4);
+	}else{
+		newFrame = pad(globalVars.counter--, 4);
+	}
+	var newImg = globalVars.prefix + newFrame + ".jpg";
+	$img.attr("src", newImg);
+}
+var interval;
+function goDesign(){
+	mouseMoveOff();
+	globalVars.forward = false;
+	globalVars.reverseOnce = true;
+	interval = setInterval( updateImg, 30);
+}
+function animate(to, speed){
+	interval = setInterval( function(){ frameUpdate(false, to) }, speed || 30);
+}
+function goInteraction(){
+	globalVars.reverseOnce = false;
+	animate(70, 15);
+	mouseMoveOn();
+}
 
-	$(document).mousemove(function(event){
-	  var xPercent = (event.pageX / $(document).width()) * 100;
-	  var normalizedNum = Math.floor(xPercent * 0.79);
-	  //console.log(normalizedNum);
-	  counter = normalizedNum ;
-	  updateImg()
-	})
+$('#designBtn').click( goDesign );
+$('#interactionBtn').click( goInteraction );
 
-
-})(jQuery)
-
-
-
-// document.addEventListener('DOMContentLoaded', function(){
-//     var img = document.getElementById('bgSequence');
-//     var canvas = document.getElementById('c');
-//     var context = canvas.getContext('2d');
-//     var btn = getElementById("cambiar");
-
-//     var cw = 1280;
-//     var ch = 720;
-//     canvas.width = cw;
-//     canvas.height = ch;
-
-//     btn.addEventListener('click', function(){
-//         draw(img,context,cw,ch);
-//     },false);
-
-// },false);
-
-// function draw(img,c,w,h) {
-//     //if(v.paused || v.ended) return false;
-//     c.drawImage(v,0,0,w,h);
-//     setTimeout(draw,20,v,c,w,h);
-// }
+animate(0)
 
 
 /*************************************************************************
