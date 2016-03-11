c2668b1ab4040ae281e0c6b5a82ba8f6690e7a42
Oscar Crespo <crespo.krw@gmail.com>
2016-03-11 02:12:41 -0500

    production ready

--- shared/js/main.js
+++ shared/js/main.js
@@ -5,6 +5,7 @@ smoothScroll.init({
 
 var $img = $("#bgSequence");
 var $win = $(window);
+var $tittles = $('#homeButtons');
 
 function documentDimensions() {
 	globalVars.$winWidth = $win.width();
@@ -19,17 +20,17 @@ window.onresize = function(){
 function mouseMoveOn(){
 	$(document).on( 'mousemove', mouseListener );
 }
-//mouseMoveOn();
+
 
 function mouseMoveOff(){
 	$(document).unbind('mousemove', mouseListener);
 }
 
 function mouseListener() {
-	var xPercent = (event.pageX / globalVars.$winWidth) * 100;
-	var yPercent = (event.pageY / globalVars.$winHeight) * 100;
+	globalVars.xPercent = (event.pageX / globalVars.$winWidth) * 100;
+	globalVars.yPercent = (event.pageY / globalVars.$winHeight) * 100;
 
-	mouseMoveFunctions( xPercent, yPercent );
+	mouseMoveFunctions( globalVars.xPercent, globalVars.yPercent );
 }
 
 function mouseMoveFunctions( x, y ) {
@@ -38,7 +39,7 @@ function mouseMoveFunctions( x, y ) {
 	console.log(x, y);
 
 	globalVars.counter = floorX ;
-
+	updateText();
 	updateImg()
 }
 
@@ -49,6 +50,7 @@ function updateImg() {
 		if (globalVars.counter == globalVars.frames-1) globalVars.forward = false;
 	}else if(globalVars.counter <= globalVars.frames && !globalVars.forward ) {
 		newNumber = pad(globalVars.counter--, 4);
+		// Reverse animation
 		if ( globalVars.reverseOnce && globalVars.counter == 0) {
 			clearInterval(interval);
 			globalVars.reverseOnce = false;
@@ -59,40 +61,60 @@ function updateImg() {
 	var newImg = globalVars.prefix + newNumber + ".jpg";
 	$img.attr("src", newImg);
 }
-function frameUpdate(forward, stop) {
-	var newFrame;
-	if ( globalVars.counter == stop) { 
-		clearInterval(interval);
-		return
-	}
-	if (forward) {
-		newFrame = pad(globalVars.counter++, 4);
+function updateText(){
+	var xReduced = Math.floor(globalVars.xPercent * -0.2) + 5;
+	var yReduced = Math.floor(globalVars.yPercent * -0.2) + 15;
+	var scaleReduced = globalVars.xPercent * 0.0041 + 0.6;
+	//console.log(scaleReduced);
+
+	if (globalVars.xPercent > 50) {
+		$tittles.find('#designBtn').removeClass('highlighted');
+		$tittles.find('#interactionBtn').addClass('highlighted');
 	}else{
-		newFrame = pad(globalVars.counter--, 4);
+		$tittles.find('#interactionBtn').removeClass('highlighted');
+		$tittles.find('#designBtn').addClass('highlighted');
 	}
-	var newImg = globalVars.prefix + newFrame + ".jpg";
-	$img.attr("src", newImg);
+	$tittles.find('h2').css({
+		transform: 'perspective(400px) rotateX('+ yReduced +'deg) rotateY('+ xReduced +'deg) scale3d('+ scaleReduced +', '+ scaleReduced +', 1 )'
+	});
 }
+// function frameUpdate(forward, stop) {
+// 	var newFrame;
+// 	if ( globalVars.counter == stop) { 
+// 		clearInterval(interval);
+// 		return
+// 	}
+// 	if (forward) {
+// 		newFrame = pad(globalVars.counter++, 4);
+// 	}else{
+// 		newFrame = pad(globalVars.counter--, 4);
+// 	}
+// 	var newImg = globalVars.prefix + newFrame + ".jpg";
+// 	$img.attr("src", newImg);
+// }
 var interval;
-function goDesign(){
-	mouseMoveOff();
-	globalVars.forward = false;
-	globalVars.reverseOnce = true;
-	interval = setInterval( updateImg, 30);
-}
-function animate(to, speed){
-	interval = setInterval( function(){ frameUpdate(false, to) }, speed || 30);
-}
-function goInteraction(){
-	globalVars.reverseOnce = false;
-	animate(70, 15);
+// function goDesign(){
+// 	mouseMoveOff();
+// 	globalVars.forward = false;
+// 	globalVars.reverseOnce = true;
+// 	interval = setInterval( updateImg, 30);
+// }
+// function animate(to, speed){
+// 	interval = setInterval( function(){ frameUpdate(false, to) }, speed || 30);
+// }
+// function goInteraction(){
+// 	globalVars.reverseOnce = false;
+// 	animate(70, 15);
+// 	mouseMoveOn();
+// }
+
+// $('#designBtn').click( goDesign );
+// $('#interactionBtn').click( goInteraction );
+
+//interval = setInterval(  updateImg , 25);
+if ($win.width()>1024) {
 	mouseMoveOn();
 }
-
-$('#designBtn').click( goDesign );
-$('#interactionBtn').click( goInteraction );
-
-animate(0)
 
 
 /*************************************************************************
