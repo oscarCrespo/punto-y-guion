d27f55be9529e9d876367a1bc3299ed93e2ddbfb
Oscar Crespo <crespo.krw@gmail.com>
2016-03-15 03:56:49 -0400

    Simplified feature, two states in the landing

--- shared/js/main.js
+++ shared/js/main.js
@@ -4,143 +4,98 @@ smoothScroll.init({
 });
 document.getElementById("loader").style.display="none";
 document.getElementById("body").className = "ready";
-
-var $imgContainer = $("#imgsBG");
-var $imgs = $('.bgSequence');
-var $win = $(window);
-var $tittles = $('#homeButtons');
-
-function documentDimensions() {
-	globalVars.$winWidth = $win.width();
-	globalVars.$winHeight = $win.height();
-}
-documentDimensions();
-
-window.onresize = function(){
-	documentDimensions();
-}
-
-function mouseMoveOn(){
-	$(document).on( 'mousemove', mouseListener );
-}
+(function($){
+	var $winWidth, 
+		$winHeight,
+		xPercent,
+		yPercent;
 
 
-function mouseMoveOff(){
-	$(document).unbind('mousemove', mouseListener);
-}
 
-function mouseListener() {
-	globalVars.xPercent = (event.pageX / globalVars.$winWidth) * 100;
-	globalVars.yPercent = (event.pageY / globalVars.$winHeight) * 100;
+	var $imgContainer = $("#imgsBG");
+	var $images = $imgContainer.find('.bgImg');
+	var $win = $(window);
+	var $tittles = $('#homeButtons');
 
-	mouseMoveFunctions( globalVars.xPercent, globalVars.yPercent );
-}
+	var $design = $imgContainer.find("#bgDesign");
+	var $interaction = $imgContainer.find("#bgInteraction");
 
-function mouseMoveFunctions( x, y ) {
-	var floorX = Math.floor(x * 0.80);
-	var floorY = Math.floor(y * 0.1);
-	console.log(x, y);
+	function getDimentions() {
+		$winWidth = $win.width();
+		$winHeight = $win.height();
+	}
+	getDimentions();
 
-	globalVars.counter = floorX ;
-	updateText();
-	updateImg()
-}
+	window.onresize = function(){
+		getDimentions();
+		switchLandingBg();
+	}
 
-// function updateImg() {
-// 	var newNumber;
-// 	if( globalVars.counter <= globalVars.frames-1 && globalVars.forward){
-// 		newNumber = pad(globalVars.counter++, 4);
-// 		if (globalVars.counter == globalVars.frames-1) globalVars.forward = false;
-// 	}else if(globalVars.counter <= globalVars.frames && !globalVars.forward ) {
-// 		newNumber = pad(globalVars.counter--, 4);
-// 		// Reverse animation
-// 		if ( globalVars.reverseOnce && globalVars.counter == 0) {
-// 			clearInterval(interval);
-// 			globalVars.reverseOnce = false;
-// 		}
-// 		if (globalVars.counter == 0) globalVars.forward = true;
-// 	}
-// 	//console.log( globalVars.counter );
-// 	var newImg = globalVars.prefix + newNumber + ".jpg";
-// 	$img.attr("src", newImg);
-// }
-var $sequence = $('.bgSequence');
-console.log($sequence.length)
-function updateImg() {
-	var newNumber;
-	if( globalVars.counter <= globalVars.frames-1 && globalVars.forward){
-		newNumber = globalVars.counter++;
-		if (globalVars.counter == globalVars.frames-1) globalVars.forward = false;
-	}else if(globalVars.counter <= globalVars.frames && !globalVars.forward ) {
-		newNumber = globalVars.counter--;
-		// Reverse animation
-		if ( globalVars.reverseOnce && globalVars.counter == 0) {
-			clearInterval(interval);
-			globalVars.reverseOnce = false;
+	function mouseMoveOn(){
+		$(document).on( 'mousemove', mouseListener );
+	}
+
+
+	function mouseMoveOff(){
+		$(document).unbind('mousemove', mouseListener);
+	}
+
+	function mouseListener() {
+		xPercent = (event.pageX / $winWidth) * 100;
+		yPercent = (event.pageY / $winHeight) * 100;
+
+		mouseMoveFunctions( xPercent, yPercent );
+	}
+
+	function mouseMoveFunctions( x, y ) {
+		var floorX = Math.floor(x * 0.80);
+		var floorY = Math.floor(y * 0.1);
+		//console.log(x, y);
+
+		//counter = floorX ;
+		updateField();
+	}
+
+	//$imgContainer.find(targetClass).addClass("-top");
+
+	function changeLanding( field ) {
+		var $bgActive = field == 'interaction' ? $interaction : $design ;
+		$images.removeClass('-top');
+		$bgActive.addClass('-top');
+
+	}
+
+	function updateField(){
+		var xReduced = Math.floor(xPercent * 0.2) - 20;
+		var yReduced = Math.floor(yPercent * -0.2) + 10;
+		var scaleReduced = xPercent * 0.0041 + 0.6;
+		//console.log(scaleReduced);
+
+		if (xPercent > 50) {
+			$tittles.find('#designBtn').removeClass('highlighted');
+			$tittles.find('#interactionBtn').addClass('highlighted');
+			changeLanding('interaction');
+		}else{
+			$tittles.find('#interactionBtn').removeClass('highlighted');
+			$tittles.find('#designBtn').addClass('highlighted');
+			changeLanding();
 		}
-		if (globalVars.counter == 0) globalVars.forward = true;
+		$tittles.find('h2').css({
+			transform: 'perspective(400px) rotateX('+ yReduced +'deg) rotateY('+ xReduced +'deg)'
+		});
 	}
-	//console.log( globalVars.counter );
-	$sequence.removeClass('-top');
-	var targetClass = '.seq'+newNumber;
-	console.log(targetClass);
-	$imgContainer.find(targetClass).addClass("-top");
-}
-function updateText(){
-	var xReduced = Math.floor(globalVars.xPercent * -0.2) + 5;
-	var yReduced = Math.floor(globalVars.yPercent * -0.2) + 15;
-	var scaleReduced = globalVars.xPercent * 0.0041 + 0.6;
-	//console.log(scaleReduced);
 
-	if (globalVars.xPercent > 50) {
-		$tittles.find('#designBtn').removeClass('highlighted');
-		$tittles.find('#interactionBtn').addClass('highlighted');
-	}else{
-		$tittles.find('#interactionBtn').removeClass('highlighted');
-		$tittles.find('#designBtn').addClass('highlighted');
+	function switchLandingBg(){
+		if ($win.width()>1024) {
+			mouseMoveOn();
+		}else{
+			mouseMoveOff();
+		}
 	}
-	$tittles.find('h2').css({
-		transform: 'perspective(400px) rotateX('+ yReduced +'deg) rotateY('+ xReduced +'deg) scale3d('+ scaleReduced +', '+ scaleReduced +', 1 )'
-	});
-}
-// function frameUpdate(forward, stop) {
-// 	var newFrame;
-// 	if ( globalVars.counter == stop) { 
-// 		clearInterval(interval);
-// 		return
-// 	}
-// 	if (forward) {
-// 		newFrame = pad(globalVars.counter++, 4);
-// 	}else{
-// 		newFrame = pad(globalVars.counter--, 4);
-// 	}
-// 	var newImg = globalVars.prefix + newFrame + ".jpg";
-// 	$img.attr("src", newImg);
-// }
-var interval;
-// function goDesign(){
-// 	mouseMoveOff();
-// 	globalVars.forward = false;
-// 	globalVars.reverseOnce = true;
-// 	interval = setInterval( updateImg, 30);
-// }
-// function animate(to, speed){
-// 	interval = setInterval( function(){ frameUpdate(false, to) }, speed || 30);
-// }
-// function goInteraction(){
-// 	globalVars.reverseOnce = false;
-// 	animate(70, 15);
-// 	mouseMoveOn();
-// }
-
-// $('#designBtn').click( goDesign );
-// $('#interactionBtn').click( goInteraction );
-
-//interval = setInterval(  updateImg , 25);
-if ($win.width()>1024) {
-mouseMoveOn();
-}
-
+	return{
+		switchLandingBg : switchLandingBg()
+	}
+})(jQuery);
 
 /*************************************************************************
 *************************************************************************
