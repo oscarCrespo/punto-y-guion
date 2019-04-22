7624361723460fe1ecce8c198c5d4b9a42a28d51
Oscar Crespo <oscar.cornejo@apiture.com>
2019-04-22 10:23:57 -0400

    Fixed SmoothScrolling issue added as package

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-var jQuery = require('jquery');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var main = require('./js/base');
-var ProjectsModule = require('./components/ProjectsModule');
+var jQuery = require("jquery");
+var React = require("react");
+var ReactDOM = require("react-dom");
+var mainApp = require("./js/base");
+var ProjectsModule = require("./components/ProjectsModule");
+var SmoothScroll = require("smooth-scroll");
 
 class ProjectsApp extends React.Component {
-  render(){
-    return(
-        <ProjectsModule />
-    )
+  componentDidMount() {
+    var scroll = new SmoothScroll('a[href*="#"]', {
+      speed: 700,
+      easing: "easeInOutCubic"
+    });
+    mainApp(jQuery);
+  }
+  render() {
+    return <ProjectsModule />;
   }
 }
-ReactDOM.render(<ProjectsApp />, document.getElementById('projectsApp'));
+ReactDOM.render(<ProjectsApp />, document.getElementById("projectsApp"));
